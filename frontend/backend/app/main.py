361a187bfdc740987efb57e63352aab9046dddd8
Faith Gutierrez <gutierrezfaithgrace7@gmail.com>
2025-04-24 15:21:50 +0800

    frontend changes

---
+++ frontend/backend/app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.database import SessionLocal, engine, Base
+from app import crud, schemas
+from app.models import Todo
+
+Base.metadata.create_all(bind=engine)
+    
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/todos", response_model=list[schemas.TodoOut])
+def read_todos(db: Session = Depends(get_db)):
+    return crud.get_todos(db)
+
+@app.post("/todos", response_model=schemas.TodoOut)
+def add_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.create_todo(db, todo)
+
+@app.put("/todos/{todo_id}", response_model=schemas.TodoOut)
+def update(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(get_db)):
+    return crud.update_todo(db, todo_id, todo)
+
+@app.delete("/todos/{todo_id}", response_model=schemas.TodoOut)
+def delete(todo_id: int, db: Session = Depends(get_db)):
+    return crud.delete_todo(db, todo_id)
